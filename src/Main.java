1f539ef6bc9c198d734e62b910b7c8c71e87041f
НафНафДурикович <yana88yana99@gmail.com>
2024-03-24 23:07:21 +0300

    Выполненно Задача №2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-       var dog=8.0;
+       var dog = 8.0;
        System.out.println(dog);
-       var cat=3.6;
+       dog = dog + 4;
+        System.out.println(dog);
+       var cat = 3.6;
        System.out.println(cat);
-       var paper=763789;
+       cat = cat + 4;
+        System.out.println(cat);
+       var paper = 763789;
+        System.out.println(paper);
+        paper = paper + 4;
         System.out.println(paper);
     }
 }
